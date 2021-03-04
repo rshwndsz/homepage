5a8c526710d26a818abf47a75576094efbebf539
Russel Shawn <30742639+rshwndsz@users.noreply.github.com>
2021-03-04 10:23:57 +0000

    Add a redirect page to nightsight

---
+++ src/pages/nightsight.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+ 
+export class Nightsight extends React.Component {
+    componentDidMount() {
+        window.location.replace("https://www.notion.so/rshwndsz/Major-Project-8d9bb43c8212493f8ad538270b287553");
+    }
+    render() {
+        return (
+            <Layout>
+                <SEO title="Nightsight" />
+            </Layout>
+        )
+    }
+}
+
+export default Nightsight
+
+export const pageQuery = graphql`
+query {
+    site {
+        siteMetadata {
+            title
+        }
+    }
+}
+`
